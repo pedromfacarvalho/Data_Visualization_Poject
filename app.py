0500572cd65e0d900cab45b49af528bbe0662fcf
pedromfacarvalho <56802327+pedromfacarvalho@users.noreply.github.com>
2020-01-15 23:54:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,128 @@
 import dash
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
 import pandas as pd
-import numpy as np
-import plotly.express as px
+import warnings,os
+import gunicorn
 
-# Dataset Processing
+warnings.filterwarnings('ignore')
+location_data=pd.read_pickle("final_reduced_df.pkl")
+# location_data=location_data.drop(['accuracy', 'activity', 'altitude','heading','timestamp', 'velocity', 'verticalAccuracy'],axis=1)
+# location_data = location_data.groupby(['person', pd.Grouper(freq='D'),'latitude','longitude'],as_index=False,sort=False).size().reset_index(drop=False)
+# #grouped_mean = location_data.groupby(['person', pd.Grouper(freq='D')],sort=False)['latitude','longitude'].mean().reset_index(drop=False)
+# location_data=location_data.drop([0],axis=1)
+# location_data['year'] = location_data['datetime'].dt.year
 
-df_emissions = pd.read_csv('emission_full.csv')
+name_options = [dict(label=name, value=name) for name in location_data['person'].unique()]
+year_options = [dict(label=year, value=year) for year in location_data['year'].unique()]
+initial_year = list(range(min(location_data['year']),max(location_data['year'])))
 
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-df_emission_0 = df_emissions.loc[df_emissions['year']==2000]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# Building our Graphs
+app.layout = html.Div([
 
-data_choropleth = dict(type='choropleth',
-                       locations=df_emission_0['country_name'],  #There are three ways to 'merge' your data with the data pre embedded in the map
-                       locationmode='country names',
-                       z=np.log(df_emission_0['CO2_emissions']),
-                       text=df_emission_0['country_name'],
-                       colorscale='inferno',
-                       colorbar=dict(title='CO2 Emissions log scaled')
-                      )
+    html.Div([
+        html.H1('Google Location History')
+    ], className='Title'),
 
-layout_choropleth = dict(geo=dict(scope='world',  #default
-                                  projection=dict(type='orthographic'
-                                                 ),
-                                  #showland=True,   # default = True
-                                  landcolor='black',
-                                  lakecolor='white',
-                                  showocean=True,   # default = False
-                                  oceancolor='azure'
-                                 ),
-                         
-                         title=dict(text='World Choropleth Map',
-                                    x=.5 # Title relative position according to the xaxis, range (0,1)
-                                   )
-                        )
+    html.Div([
 
-fig = go.Figure(data=data_choropleth, layout=layout_choropleth)
+        html.Div([
+            html.Label('Person'),
+            dcc.Dropdown(
+                id='person_drop',
+                options=name_options,
+                value=['Ben','Carolina','Leo','Pedro'],
+                multi=True
+            ),
 
-# The App itself
+            html.Br(),
 
-app = dash.Dash(__name__)
+            html.Label('Year Choice'),
+            dcc.Slider(
+                id='year-slider',
+                min=location_data['year'].min(),
+                max=location_data['year'].max(),
+                value=location_data['year'].min(),
+                marks={str(year): str(year) for year in location_data['year'].unique()},
+                step=None
+            )
 
-server = app.server
+        ], className='column1 pretty'),
+        html.Div([dcc.Graph(id='graph-with-slider')], className='column2 pretty')
+    ], className='row'),
 
-app.layout = html.Div(children=[
-    html.H1(children='My DashBoard'),
+    html.Div([
+        html.Br(),
+        # html.Div([dcc.Graph(id='heatmap')], className='column1 pretty'),
 
-    html.Div(children='''
-        Example of html Container
-    '''),
+        html.Div([dcc.Graph(id='heatmap')], className='column3 pretty')
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    ], className='row'),
+
 ])
+
+@app.callback(
+    [Output('graph-with-slider', 'figure'),
+    Output('heatmap','figure')],
+    [Input('year-slider', 'value'),
+     Input('person_drop','value')])
+
+def update_figure(selected_year,person_selected):
+    filtered_df = location_data[location_data.year == selected_year]
+    traces = []
+    filtered_df=filtered_df[filtered_df.person.isin(person_selected)]
+    for i in filtered_df.person.unique():
+        df_by_person= filtered_df[filtered_df['person'] == i]
+        if i =='Leo':
+            color_person='blue'
+        elif i=='Pedro':
+            color_person='purple'
+        elif i=='Carolina':
+            color_person='red'
+        else:
+            color_person='green'
+        traces.append(dict(type='scattermapbox',
+                         mode="markers",
+                         lon = df_by_person['longitude'],
+                         lat = df_by_person['latitude'],
+                         marker=dict(
+                         size =10,
+                         # size=df_by_person.groupby(['longitude','latitude'])['datetime'].count(),
+                         sizeref=0.9,
+                         color=color_person,
+                         opacity=0.9,
+                         name=i,
+                         labelgrup=i
+                           )
+                        ))
+
+    fig = go.Figure(go.Densitymapbox(lat=filtered_df['latitude'], lon=filtered_df['longitude'],
+                                     radius=10))
+    fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    return [{
+        'data': traces,
+        'layout': dict(margin ={'l':0,'t':0,'b':0,'r':0},
+                        mapbox = {
+                        'center': {'lon': 10, 'lat': 10},
+                        'style': "stamen-terrain",
+                        'center': {'lon': -20, 'lat': -20},
+                        'zoom': 1},
+                        legend = dict(bordercolor='rgb(100,100,100)',
+                                      borderwidth=2,
+                                      itemclick='toggleothers', # when you are clicking an item in legend all that are not in the same group are hidden
+                                      x=0.91,
+                                      y=1)
+    )
+    },fig]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
